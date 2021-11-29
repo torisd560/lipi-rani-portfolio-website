b78c93a5986139bd622641572345e2fbd1265930
torisd560 <mitalisd123@gmail.com>
2021-11-29 16:44:58 +0600

    added portfolio routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../src/Pages/Home/Home'
+import About from '../src/Pages/About/About'
+import Works from '../src/Pages/Works/Works'
+import Contact from '../src/Pages/Contact/Contact'
+import NotFound from '../src/Pages/NotFound/NotFound'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home></Home>} />
+          <Route path='/home' element={<Home></Home>} />
+          <Route path='about' element={<About></About>} />
+          <Route path='work' element={<Works></Works>} />
+          <Route path='contact' element={<Contact></Contact>} />
+          <Route path='*' element={<NotFound></NotFound>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Lipi Rani</h1>
+        </div>
+    );
+};
+
+export default Header;
