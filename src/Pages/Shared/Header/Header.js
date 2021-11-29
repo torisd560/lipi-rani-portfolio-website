a2da6ff63dffd1da777d40dd3551eea512cec17b
torisd560 <mitalisd123@gmail.com>
2021-11-29 22:47:55 +0600

    added all

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,7 +1,6 @@
-import Button from '@restart/ui/esm/Button';
 import React from 'react';
 import lipiImg from '../../../images/portfolio.jpg'
-
+import './Header.css'
 const Header = () => {
     return (
         <div className='text-center' >
@@ -9,7 +8,6 @@ const Header = () => {
                 <img src={lipiImg} alt="" className='img-fluid' style={{ verticalAlign: 'super' }} />
                 <h2 className='p-1 w-100 text-white' style={{ backgroundColor: '#00A3E1', position: 'absolute', bottom: 0, left: 0 }}>LIPI RANI</h2>
             </div>
-            <a title = "_blank" href="https://drive.google.com/file/d/1t9yGU7-_TSGQObXSRY0-L4LJpAkqqEpg/view?usp=sharing">Resume</a>
         </div>
     );
 };
