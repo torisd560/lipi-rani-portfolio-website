f980ec444973df3697b116d299cb8d75faa05be0
torisd560 <mitalisd123@gmail.com>
2022-02-23 12:35:44 +0600

    [MOD] resume link

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -9,14 +9,14 @@ const Header = () => {
             <Navbar collapseOnSelect expand="lg" bg="transparent" variant="light">
                 <Container>
                     <Navbar.Brand className='navbarBrand text-white'>L</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" className = 'bg-white' />
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" className='bg-white' />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto custom-nav align-items-center">
                             <Nav.Link as={Link} to="/home" style={{ color: '#dde1e7' }}>Home</Nav.Link>
                             <Nav.Link as={Link} to="/about" style={{ color: '#dde1e7' }}>About</Nav.Link>
                             <Nav.Link as={Link} to="/work" style={{ color: '#dde1e7' }}>Works</Nav.Link>
                             <Nav.Link as={Link} to="/blog" style={{ color: '#dde1e7' }}>Blogs</Nav.Link>
-                            <a className='resume-btn' target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1t9yGU7-_TSGQObXSRY0-L4LJpAkqqEpg/view?usp=sharing">Resume</a>
+                            <a className='resume-btn' target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1hDHuPkvBVF5cZQ5coYFO4TfoBnyoMzMU/view?usp=drivesdk">Resume</a>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
